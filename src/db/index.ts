d05df7135dd7d2c030ea4321e7c66c71130a1499
vemaarnandha <exlaluna02@gmail.com>
2026-05-29 08:58:10 +0700

    Commit pertama folder saya

--- src/db/index.ts
+++
@@ -1,8 +0,0 @@
-import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client";
-
-const client = createClient({
-  url: "file:database.db", // file SQLite disimpan di root project
-});
-
-export const db = drizzle(client);
